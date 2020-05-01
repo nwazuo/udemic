d91f19246d5be0e58cd19bb66178ca372d27ab79
Chizo <chizonwazuo@yahoo.com>
2020-05-01 11:36:08 +0100

    Testing out heroku deploy on new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import { Text } from "@chakra-ui/core";
+import { openUploadWidget } from "./utils/CloudinaryService";
+
+let loginStatus;
+
+const googleResponse = async response => {
+  loginStatus = await response;
+  console.log(loginStatus);
+};
 
 function App() {
+  const openCloudinaryWidget = () => {
+    openUploadWidget(
+      { cloudName: "udemic", uploadPreset: "sample" },
+      (error, result) => {
+        if (!error) {
+          console.log(result);
+        } else {
+          console.log(error);
+        }
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login responseGoogle={googleResponse} />
+      <div>
+        <button onClick={openCloudinaryWidget}>Open Cloudinary Widget</button>
+      </div>
+      <Authenticate />
     </div>
+  );
+}
+
+function Authenticate() {
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {});
+
+  if (status === null) {
+    return (
+      <Text fontSize="4xl" fontWeight="bold">
+        Loading...
+      </Text>
+    );
+  }
+
+  return (
+    <Text fontSize="4xl" fontWeight="bold">
+      {status}
+    </Text>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Router } from "@reach/router";
+import Chakra from "./Chakra";
+import { CSSReset, theme } from "@chakra-ui/core";
+import { ThemeProvider } from "emotion-theming";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Router>
+        <App path="/" />
+        <Chakra path="/chakra" />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
